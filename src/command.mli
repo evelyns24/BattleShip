3800965d6f6a84ed72f1a7c17ad1260f505c6cf2
Evelyn <es828@cornell.edu>
2023-04-29 08:04:48 -0400

    Update specs for Command.parse

--- src/command.mli
+++ src/command.mli
@@ -54,15 +54,23 @@ val parse : string -> command
     word (i.e., consecutive sequence of non-space characters) of [str] becomes
     the verb. The rest of the words, if any, become the object phrase. Examples:
 
-    - [parse "    go   clock   tower   "] is [Go \["clock"; "tower"\]]
-    - [parse "quit"] is [Quit].
+    - [parse "        hit    1  5   "] is [Hit \(1,5\)]
+    - [parse " quit   "] is [Quit]
+    - [parse "    move   Cruiser    1  5   "] is [Move \{"Cruiser"; 1; 5\}]
+    - [parse "    rotate   Destroyer    2  1   "] is
+      [Rotate \{"Destroyer"; 2; 1\}]
 
     Requires: [str] contains only alphanumeric (A-Z, a-z, 0-9) and space
     characters (only ASCII character code 32; not tabs or newlines, etc.).
 
     Raises: [Empty] if [str] is the empty string or contains only spaces.
 
-    Raises: [Malformed] if the command is malformed. A command is malformed if
-    the verb is neither "quit" nor "go", or if the verb is "quit" and there is a
-    non-empty object phrase, or if the verb is "go" and there is an empty object
-    phrase.*)
+    Raises: [Malformed] if the command is malformed. A command is not malformed
+    only if:
+
+    - the verb is "quit" and there is an empty object phrase; or
+    - the verb is "move"/"rotate" and there is an object phrase of 3 elements,
+      where the first element is a string and both the second and third are
+      integers; or
+    - the verb is "hit" and there is an object phrase of size 2, where both
+      elements are integers. *)
